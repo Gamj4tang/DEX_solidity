c73978bb01b82e9ba1d95b961a0c6598dc2a38ef
WOON <youngwoon.kim@dream-academy.io>
2023-03-19 06:48:29 +0900

    이벤트 데이터 처리 변경+ERC20 transfer 유동성 중복 주석 처리

--- src/interfaces/IDex.sol
+++ src/interfaces/IDex.sol
@@ -14,11 +14,14 @@ interface IDex {
     function swap(uint256 tokenXAmount, uint256 tokenYAmount, uint256 tokenMinimumOutputAmount) external returns (uint256 outputAmount);
     function addLiquidity(uint256 tokenXAmount, uint256 tokenYAmount, uint256 minimumLPTokenAmount) external returns (uint256 LPTokenAmount);
     function removeLiquidity(uint256 LPTokenAmount, uint256 minimumTokenXAmount, uint256 minimumTokenYAmount) external returns(uint256 tokenXAmount, uint256 tokenYAmount);
-    function transfer(address to, uint256 lpAmount) external returns (bool);
+    // function transfer(address to, uint256 lpAmount) external returns (bool);
 
     // event
-    event Swap(address indexed from, address indexed to, uint256 amount);
-    event AddLiquidity(address indexed from, uint256 amount);
+    event AddLiquidity(address indexed provider, uint indexed amountX, uint indexed amountY);
+    event RemoveLiquidity(address indexed provider, uint indexed amountX, uint indexed amountY);
+    event Swap(address indexed trader, uint indexed inputAmount, uint indexed outputAmount);
 }
+
+
 
 
